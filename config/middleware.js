4f1cc0def9de11f05aed24be60e9e3df95888e3f
Brian Graham <graham.briank@gmail.com>
2015-11-16 14:37:22 -0800

    add server files

--- config/middleware.js
+++ config/middleware.js
@@ -1,8 +1,19 @@
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 module.exports = function (app, express) {
   var userRouter = express.Router();
+  var postRouter = express.Router();
+
   app.use(morgan('dev'));
+  app.use(bodyParser.urlencoded({extended : true}));
+  app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../client'));
+
+  app.use('/api/users', userRouter);
+
+  app.use('/api/posts', postRouter);
+
+  // add requires for userRoutes and postRoutes, inject the routers into each
 
 };
